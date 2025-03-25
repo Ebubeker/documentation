addafc2790738688eccabd36a3e48b47cdc66530
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2025-03-25 22:07:37 +0100

    fix: documentation almost finished

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import { redirect } from 'next/navigation'
 import Link from 'next/link';
 
 export default function HomePage() {
+
+  redirect('/docs')
+
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,25 +1,14 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
+import LogoDark from '@/lib/assets/logo-white.png';
+import LogoWhite from '@/lib/assets/logo.png';
 
-/**
- * Shared layout configurations
- *
- * you can customise layouts individually from:
- * Home Layout: app/(home)/layout.tsx
- * Docs Layout: app/docs/layout.tsx
- */
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <Image src={LogoDark} alt='Dark mode logo.' className='hidden dark:block' width={100}/>
+        <Image src={LogoWhite} alt='Light mode logo.' className="dark:hidden" width={100}/>                                
       </>
     ),
   },
